45a924ac3c7b21a0d9caf109d089d191d412ca86
Willie Rose <willierose87@gmail.com>
2021-01-10 10:33:53 -0600

    ignore file and ex11

---
+++ ex11.py
@@ -0,0 +1,11 @@
+print("How old are you? ")
+age = input()
+print("How tall are you? ", end='')
+height = input()
+print("How much do you weigh? ", end='')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+x = input("How old are you? ")
+print(f"You are {x} and you are grown", x)
