153b98ab97f87759dcda0a8734ad3ada75fe6d67
Willie Rose <willierose87@gmail.com>
2021-01-10 10:21:47 -0600

    Initial commit

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+#print("Hello Again")
+#print("I like typing like this")
+#print("This is fun.")
+#print("Yay! Printing.")
+#print("I'd much rather 'not'.")
+#print('I "said" do not touch this.')
+#print("My name is Willie Rose")
